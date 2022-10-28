77a76e8184c35522446149a33e67247d7cdb0c5d
Vadym <bilvad1@gmail.com>
2022-10-28 16:09:06 +0300

    New commit

--- spec/jasmine_examples/ProductListPageSpec.js
+++ spec/jasmine_examples/ProductListPageSpec.js
@@ -24,18 +24,18 @@ describe("Verify product Listing Page", function() {
         expect(answer).toBe('Women')
     });
 
-    it("verify Size(iFrame)", async function() {  //iFrame, scroll, executeScript
+    xit("verify Size(iFrame)", async function() {  //iFrame, scroll, executeScript
         await (await ListingPage.selectSize('S')).click()
         await ListingPage.driver.sleep(15000)
         let answer = await myAssert.checkSizes('S')
         expect(answer).toBe(true)
     });
 
-    xit("verify Color", async function() {
+    it("verify Color", async function() {
         const {until, By} = require('selenium-webdriver');
         await ListingPage.selectColor('White', 'Black')
         await ListingPage.driver.wait(until.elementLocated(By.id(`enabled_filters`)), 20000);
-        let variableForCheck = await ListingPage.allProductsHaveColor_v2('white', 'black')
+        let variableForCheck = await myAssert.allProductsHaveColor_v2('white', 'black')
         expect(variableForCheck).toBe(true)
     });
 
