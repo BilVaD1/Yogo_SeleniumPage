2b182ed21dc3eb96d169ed2e6da5d6490454957a
Vadym <bilvad1@gmail.com>
2022-10-30 12:04:23 +0200

    Added assertions

--- spec/jasmine_examples/ProductListPageSpec.js
+++ spec/jasmine_examples/ProductListPageSpec.js
@@ -1,5 +1,9 @@
 const ListingPage = require('../../lib/pom/productListing.page')
 const myAssert = require('../../lib/helpers/myAssertions')
+const chai = require('chai');
+const chaiMatchPattern = require('chai-match-pattern');
+chai.use(chaiMatchPattern);
+const _ = chaiMatchPattern.getLodashModule();
 
 describe("Verify product Listing Page", function() {
     
@@ -25,28 +29,39 @@ describe("Verify product Listing Page", function() {
     });
 
     xit("verify Size(iFrame)", async function() {  //iFrame, scroll, executeScript
-        await (await ListingPage.selectSize('S')).click()
-        await ListingPage.driver.sleep(15000)
+        await ListingPage.selectSize('S')
+        await ListingPage.handleFiltersBlock()
         let answer = await myAssert.checkSizes('S')
         expect(answer).toBe(true)
     });
 
-    it("verify Color", async function() {
-        const {until, By} = require('selenium-webdriver');
+    xit("verify Color", async function() {
         await ListingPage.selectColor('White', 'Black')
-        await ListingPage.driver.wait(until.elementLocated(By.id(`enabled_filters`)), 20000);
+        await ListingPage.handleFiltersBlock()
         let variableForCheck = await myAssert.allProductsHaveColor_v2('white', 'black')
         expect(variableForCheck).toBe(true)
     });
 
     xit("verify Range", async function() {
         await ListingPage.selectRange(30)
-        await ListingPage.driver.sleep(10000)
+        await ListingPage.handleFiltersBlock()
+        let lowPrice = (await ListingPage.getPriceRange())[0]
+        let highPrice = (await ListingPage.getPriceRange())[1]
+        let prices = await ListingPage.getPricesOfItems()
+        for(let price of prices){
+            chai.expect(price).isBetween(lowPrice, highPrice);
+        }
+        
     });
 
-    xit("verify sort by(Slider)", async function() {
+    it("verify sort by(Slider)", async function() {
         await ListingPage.sortByText('Price: Lowest first')
-        await ListingPage.driver.sleep(10000)
+        await ListingPage.loadingFinish()
+        let prices = await ListingPage.getPricesOfItems()
+
+        //console.log(prices, myAssert.determineOrder(prices))
+        expect(myAssert.determineOrder(prices)).toBe('ascending')
+
     });
 
   });
